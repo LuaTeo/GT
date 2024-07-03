025253fa2b1f8072362941999232b93672faac35
Luana Teodósio <luana.fadua@hotmail.com>
2024-07-03 11:48:57 -0300

    Acrescentando o Switch case

--- html-css-js/modulo_javascript/4estruturas-cond/script.js
+++ html-css-js/modulo_javascript/4estruturas-cond/script.js
@@ -26,3 +26,20 @@ else if (numero < 5)
     else {
         console.log("O número é igual a 5");
     }
+
+    let modo = prompt("Digite um modo: dark, red ou blue:"); 
+    switch (modo) { 
+        case "dark": 
+            document.body.style.backgroundColor = "black";
+            document.body.style.color = "white"; 
+        break;
+        case "red": 
+            document.body.style.backgroundColor = "red"; 
+            document.body.style.color = "white"; 
+        break; case "blue": 
+            document.body.style.backgroundColor = "blue"; 
+            document.body.style.color = "white"; 
+        break; 
+        default: document.body.style.backgroundColor = "pink"; 
+        document.body.style.color = "black"; 
+    }
